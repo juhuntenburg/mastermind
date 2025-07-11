672854dae5a7370fe29f1b66c53d59fdf3189c8c
juhuntenburg <ju.huntenburg@gmail.com>
2025-07-11 14:41:48 +0200

    Adds feedback functions

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -25,9 +25,30 @@ class Game
       print "Invalid guess. "
     end
   end
+
+  def check_position(code, guess)
+    guess.each_with_index.map {|val, idx| code[idx] == val}
+  end
+
+  def check_color(code, guess)
+    guess.reduce(0) do |count, val|
+      (count += 1 ; code.delete_at(code.index(val))) if code.any?(val)
+      count
+    end
+  end
+
+  def feedback(guess)
+    correct_pos = check_position(code, guess)
+    correct_color = check_color(
+      code.filter.with_index { |_, idx| !correct_pos[idx] },
+      guess.filter.with_index { |_, idx| !correct_pos[idx] }
+    )
+    {position: correct_pos.count(true), color: correct_color}
+    end
 end
 
 game = Game.new
 game.create_code
+p game.code
 guess = game.guess
-puts guess
+p game.feedback(guess)
