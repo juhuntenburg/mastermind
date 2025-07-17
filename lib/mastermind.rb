f886f74b0a1b7e1ec031dc32293e72ffa979e24c
juhuntenburg <ju.huntenburg@gmail.com>
2025-07-17 10:30:44 +0200

    Finalizes the game against the computer

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,38 +1,43 @@
-# Four positions: 1, 2, 3, 4
-# Six colors: R(ed), B(lue), G(reen), Y(ellow), O(range), P(urple)
+# frozen_string_literal: true
 
-# Game class
-# For 12 rounds or until code cracked:
-## Compare guess with code
-## Return feedback: right color wrong position / right color right position
-class Game
-  attr_accessor :colors, :color_names, :code
+# Class to initiate and play the game
+class Mastermind
+  COLORS = %w[R B G Y O P].freeze
+  COLOR_NAMES = "R(ed) B(lue) G(reen) Y(ellow) O(range) P(urple)"
+  attr_accessor :code
 
   def initialize
-    @colors = %w[R B G Y O P]
-    @color_names = "R(ed) B(lue) G(reen) Y(ellow) O(range) P(urple)"
+    @code = COLORS.values_at(*4.times.map { rand(6) })
   end
 
-  def create_code
-    @code = colors.values_at(*4.times.map {rand(6)})
+  def play
+    (1..12).each do |i|
+      puts "\nROUND #{i}"
+      feedback = feedback(guess)
+      puts "Correct position: #{feedback[:position]}"
+      puts "Correct color: #{feedback[:color]}"
+      return puts "You cracked the code!" if feedback[:position] == 4
+    end
+    puts "You ran out of trials, the correct code was: #{code.join}"
   end
 
   def guess
     loop do
-      puts "Enter your guess, four characters of #{color_names}: "
-      guess = gets.chomp.upcase.gsub(/[^RBGYOP]/, "").split("")
+      puts "Enter your guess, four characters of #{COLOR_NAMES}: "
+      guess = gets.chomp.upcase.gsub(/[^RBGYOP]/, "").chars
       return guess if guess.size == 4
+
       print "Invalid guess. "
     end
   end
 
   def check_position(code, guess)
-    guess.each_with_index.map {|val, idx| code[idx] == val}
+    guess.each_with_index.map { |val, idx| code[idx] == val }
   end
 
   def check_color(code, guess)
     guess.reduce(0) do |count, val|
-      (count += 1 ; code.delete_at(code.index(val))) if code.any?(val)
+      (count += 1 and code.delete_at(code.index(val))) if code.any?(val)
       count
     end
   end
@@ -43,12 +48,9 @@ class Game
       code.filter.with_index { |_, idx| !correct_pos[idx] },
       guess.filter.with_index { |_, idx| !correct_pos[idx] }
     )
-    {position: correct_pos.count(true), color: correct_color}
-    end
+    { position: correct_pos.count(true), color: correct_color }
+  end
 end
 
-game = Game.new
-game.create_code
-p game.code
-guess = game.guess
-p game.feedback(guess)
+game = Mastermind.new
+game.play
