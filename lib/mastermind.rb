34f77708e1eec75876ac7cb369cf9e5912798236
juhuntenburg <ju.huntenburg@gmail.com>
2025-07-17 17:53:06 +0200

    Gets computer mastermind to work with random guesses

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -4,8 +4,8 @@ require_relative "mastermind/human_mastermind"
 require_relative "mastermind/computer_mastermind"
 
 
-game = HumanMastermind.new(debug: true)
-game.play
+#game = HumanMastermind.new(debug: true)
+#game.play
 
 
 game = ComputerMastermind.new
--- lib/mastermind/computer_mastermind.rb
+++ lib/mastermind/computer_mastermind.rb
@@ -2,9 +2,21 @@
 
 require_relative "base_mastermind"
 
+# Version where the human creates the code and the computer guesses
 class ComputerMastermind < BaseMastermind
 
   def play
     @code = enter_code
+    (1..12).each do |i|
+      sleep(2)
+      puts "\nROUND #{i}"
+      guess = COLORS.values_at(*4.times.map { rand(6) })
+      puts "Computer guesses #{guess.join}"
+      feedback = feedback(guess)
+      puts "Correct position: #{feedback[:position]}"
+      puts "Correct color: #{feedback[:color]}"
+      return puts "Computer cracked the code!" if feedback[:position] == 4
+    end
+    puts "Computer ran out of trials, the correct code was: #{code.join}"
   end
 end
